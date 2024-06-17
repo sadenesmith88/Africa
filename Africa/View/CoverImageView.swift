3c0917cc14033278681a13c00a523e01506aa9e8
Sade Nesmith <sadework21@gmail.com>
2024-06-16 22:24:56 -0400

    recommit

--- Africa/View/CoverImageView.swift
+++ Africa/View/CoverImageView.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct CoverImageView: View {
+  //MARK: - PROPERTIES
+
+  //MARK: - BODY
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      TabView {
+        ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+          Image("cover-lion")
+          .resizable()
+        .scaledToFit()
+        }//: LOOP
+      } //:TAB
+
+      .tabViewStyle(PageTabViewStyle())
     }
 }
 
+//MARK: - PREVIEW
+
 #Preview {
     CoverImageView()
+    .previewLayout(.fixed(width: 400, height: 400))
 }
