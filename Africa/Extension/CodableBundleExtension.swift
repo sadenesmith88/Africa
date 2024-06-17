d569c4d1086ef9ad50a5d48777a570ca08b0a756
Sade Nesmith <sadework21@gmail.com>
2024-06-16 23:05:15 -0400

    json decoder implemented

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,8 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by sade on 6/16/24.
+//
+
+import Foundation
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by sade on 6/16/24.
+//
+
+import Foundation
