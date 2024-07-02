b2744f0f577be311837a0af7a7287c608cb5d0b8
Sade Nesmith <sadework21@gmail.com>
2024-07-02 17:39:02 -0400

    grid layout

---
+++ Africa/View/AnimalGridItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalGridItemView.swift
+//  Africa
+//
+//  Created by sade on 7/2/24.
+//
+
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimalGridItemView()
+}
